b31a9422cb4ad13c99a6cc4c4e1d12d49f1a77e4
Stephen Beiter <stephenbeiter@gmail.com>
2021-02-24 20:56:12 -0800

    added User and Thought schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  friendCount: Int
+  thoughts: [Thought]
+  friends: [User]
+}
+type Reaction {
+  _id: ID
+  reactionBody: String
+  createdAt: String
+  username: String
+}
+type Thought {
+  _id: ID
+  thoughtText: String
+  createdAt: String
+  username: String
+  reactionCount: Int
+  reactions: [Reaction]
+}
+type Query {
+  users: [User]
+  user(username: String!): User
+  thoughts(username: String): [Thought]
+  thought(_id: ID!): Thought
+}
+`;
+
+module.exports = typeDefs;

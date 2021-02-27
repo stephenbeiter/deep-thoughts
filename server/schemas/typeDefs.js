88d091c4427d9ca39126308c108637a5c7891c6d
Stephen Beiter <stephenbeiter@gmail.com>
2021-02-26 19:34:33 -0800

    added mutations and authentication via JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
 type User {
   _id: ID
   username: String
@@ -9,12 +10,14 @@ type User {
   thoughts: [Thought]
   friends: [User]
 }
+
 type Reaction {
   _id: ID
   reactionBody: String
   createdAt: String
   username: String
 }
+
 type Thought {
   _id: ID
   thoughtText: String
@@ -23,11 +26,26 @@ type Thought {
   reactionCount: Int
   reactions: [Reaction]
 }
+
 type Query {
+  me: User
   users: [User]
   user(username: String!): User
   thoughts(username: String): [Thought]
   thought(_id: ID!): Thought
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addThought(thoughtText: String!): Thought
+  addReaction(thoughtId: ID!, reactionBody: String!): Thought
+  addFriend(friendId: ID!): User
+}
+
+type Auth {
+  token: ID!
+  user: User
 }
 `;
 

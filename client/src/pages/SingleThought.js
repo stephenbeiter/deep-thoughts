a2f31da64608bac551d6a74abcd222b95f34860e
Stephen Beiter <stephenbeiter@gmail.com>
2021-03-01 02:07:39 -0800

    added forms

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
+import Auth from '../utils/auth';
 
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -33,6 +35,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
